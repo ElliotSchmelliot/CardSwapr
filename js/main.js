b421c62b51ab66c207167c0697703fe6421899d6
Elliot Keder <osa28@comcast.net>
2013-12-09 14:07:03 -0800

    home page dynamic js, feature page skeleton

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 $(function() {
+	/* "Tell me more" scroll */
 	$("#more-span").click(function() {
 		$('html, body').animate({
         	scrollTop: $("#lower-content").offset().top
 		}, 2000);
 	});
 
-			//bind("load", function () { $(this).fadeIn(); });
+	/* Dynamically set lower content min-height and paddings */
+	var bodyHeight = $("body").height();
+	$("#lower-content").css("min-height", bodyHeight);
+	var infoBlocks = $(".info-block");
+	infoBlocks.height(bodyHeight / 5);
+	infoBlocks.css("margin", 0);
+	$("#lower-content").css("padding-top", bodyHeight / 10);
+	$("#lower-content").css("padding-bottom", bodyHeight / 10);
 
-		// $("body").css("background-color", "blue");
-
+	$(".full-stretch-div").height(bodyHeight);
 });
