9bae9cecc19d6c9c557211e251f8db2cc78ede47
Elliot Keder <osa28@comcast.net>
2013-12-09 23:36:14 -0800

    font work

--- js/main.js
+++ js/main.js
@@ -7,11 +7,11 @@ $(function() {
 	});
 
 	/* Dynamically set lower content min-height and paddings */
-	var bodyHeight = $("body").height();
-	$("#lower-content").css("min-height", bodyHeight);
-	var infoBlocks = $(".info-block");
-	infoBlocks.height(bodyHeight / 5);
-	infoBlocks.css("margin", 0);
-	$("#lower-content").css("padding-top", bodyHeight / 10);
-	$("#lower-content").css("padding-bottom", bodyHeight / 10);
+	// var bodyHeight = $("body").height();
+	// $("#lower-content").css("min-height", bodyHeight);
+	// var infoBlocks = $(".info-block");
+	// infoBlocks.height(bodyHeight / 5);
+	// infoBlocks.css("margin", 0);
+	// $("#lower-content").css("padding-top", bodyHeight / 10);
+	// $("#lower-content").css("padding-bottom", bodyHeight / 10);
 });
